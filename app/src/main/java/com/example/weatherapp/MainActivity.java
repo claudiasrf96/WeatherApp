b65404ffa369986724f65189e5ca51ddc56a6043
claudiasrf96 <klaudiaaf1996@hotmail.com>
2020-01-31 23:41:29 +0000

    GPS functioning

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.weatherapp;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,14 +32,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.Permission;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    WeatherReport currentCity=null;
+    WeatherReport currentCity = null;
     ArrayList<WeatherReport> citiesList = new ArrayList<>();
-    JSONObject responseJSON=null;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    String URLbase = "http://api.openweathermap.org/data/2.5/weather?";
+    String appID = "&appid=4d26ff9720cf1c7f6334eb77b08c7bd8";
+    Double latitude;
+    Double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +54,44 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ObtainGPSLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates("gps", 9000000, 0, locationListener);
+                }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_manage_cities) {
+            onManageCitiesClick();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onManageCitiesClick(){
+        //new activity
+    }
+
+    private void obtainDataFromAPI(){
         RequestQueue requestQueue= Volley.newRequestQueue(this);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET,
-                "http://api.openweathermap.org/data/2.5/weather?lat=39.7436200&lon=-8.8070500&appid=4d26ff9720cf1c7f6334eb77b08c7bd8",
+                URLbase+"lat="+latitude.toString()+"&lon="+longitude.toString()+appID,
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
@@ -71,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
                             descriptionText.setText(currentCity.getDescription());
                             humidityText.setText(currentCity.getHumidity().toString()+" %");
                             windSpeedText.setText(currentCity.getWindSpeed().toString()+" km/h");
-                            System.out.println(currentCity.toString());
+                            
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -89,29 +138,46 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(jsonObjectRequest);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void ObtainGPSLocation(){
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude=location.getLatitude();
+                longitude=location.getLongitude();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                obtainDataFromAPI();
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_manage_cities) {
-            onManageCitiesClick();
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+                return;
+            }
         }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onManageCitiesClick(){
-        //new activity
+        else {
+            locationManager.requestLocationUpdates("gps", 9000000, 0, locationListener);
+        }
     }
 }
